3e471131756745446c6f6f36a2c643d83530543c
uaesmartlink <uaesmartlink@gmail.com>
2023-01-30 19:51:37 +0400

    init and add presignup page

---
+++ lib/src/screen/auth_screen/pre_sign_up_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import '../../_route/routes.dart';
+import 'package:yoori_ecommerce/src/utils/images.dart';
+import 'package:yoori_ecommerce/src/utils/app_tags.dart';
+
+class PreSignUp extends StatelessWidget {
+  const PreSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomCard(
+              title: "Individual",
+              icon: "individual",
+              callback: () => {
+                Get.toNamed(
+                  Routes.signUp,
+                  arguments: AppTags.individualType,
+                )
+              },
+            ),
+            CustomCard(
+              title: "Company",
+              icon: "company",
+              callback: () => {
+                Get.toNamed(
+                  Routes.signUp,
+                  arguments: AppTags.companyType,
+                )
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String icon;
+  final VoidCallback callback;
+
+  const CustomCard(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: InkWell(
+          onTap: callback,
+          child: SizedBox(
+            width: 300,
+            height: 100,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/icons/$icon.svg",
+                    height: 25,
+                    width: 25,
+                  ),
+                  const SizedBox(
+                    width: 10, // <-- SEE HERE
+                  ),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,31 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:yoori_ecommerce/src/models/video_shopping_details_model.dart';
+import 'package:yoori_ecommerce/src/utils/constants.dart';
+
+
+void main() {
+  test('', () async {
+    final response = await http.get(Uri.parse("https://yoori.spagreen.net/api/v100/video-shops-details/asd?token=null&lang=null&curr=null"));
+    try {
+      var data = json.decode(response.body);
+      var videoShoppingDetailsModel = VideoShoppingDetailsModel.fromJson(data);
+      printLog("Video Type => ${videoShoppingDetailsModel.data!.video!.videoType}");
+      return videoShoppingDetailsModel;
+    } catch (e) {
+      printLog("inside catch");
+      throw Exception(e);
+    }
+
+  });
+}
