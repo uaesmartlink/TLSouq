f1219123203a9d372cf5cae63246a45653c1e4ce
uaesmartlink <uaesmartlink@gmail.com>
2023-02-17 18:21:35 +0400

    update default currency

--- lib/src/data/data_storage_service.dart
+++ lib/src/data/data_storage_service.dart
@@ -4,11 +4,13 @@ import 'package:get_storage/get_storage.dart';
 class StorageService extends GetxService {
   String? languageCode;
   String? countryCode;
-
+  String? currCode;
   Future<StorageService> init() async {
     await GetStorage.init();
     languageCode = GetStorage().read('languageCode');
     countryCode = GetStorage().read('countryCode');
+    write("currCode", "AED");
+    currCode = GetStorage().read('currCode');
     return this;
   }
   void write(String key, dynamic value) {
